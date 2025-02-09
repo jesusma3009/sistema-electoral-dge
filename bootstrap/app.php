0bd54f721a06a20f1757545d0910b9d061f1f355
Jesús Marín Almansa <jesus@jmarin.dev>
2025-02-09 13:10:25 +0100

    Configuración de cookies más segura

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,5 +4,9 @@ require_once __DIR__ . '/loadEnv.php';
 require_once __DIR__ . '/../config/database.php';
 
 session_start([
+    'name' => 'VotacionDGE_UGR',
     'cookie_lifetime' => 604800,
+    'cookie_secure' => true,
+    'cookie_httponly' => !$_ENV['APP_DEBUG'],
+    'cookie_samesite' => 'Strict'
 ]);
